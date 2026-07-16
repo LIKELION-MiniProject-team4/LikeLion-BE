5caf43f1c4a35a6a5785fcb14a081c14becd0372
jaewon9804 <kumjaewon9804@g.eulji.ac.kr>
2026-07-17 01:11:09 +0900

    [FEAT] 로그아웃 API 구현 및 AuthException 500 응답 버그 수정

--- src/main/java/com/likelion/miniproject/user/controller/response/UserResponseCode.java
+++ src/main/java/com/likelion/miniproject/user/controller/response/UserResponseCode.java
@@ -8,7 +8,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum UserResponseCode implements ResponseCode {
     SIGNUP_SUCCESS("USER_201_1", "회원가입에 성공했습니다."),
-    LOGIN_SUCCESS("USER_200_1", "로그인에 성공했습니다.");
+    LOGIN_SUCCESS("USER_200_1", "로그인에 성공했습니다."),
+    LOGOUT_SUCCESS("USER_200_2", "로그아웃에 성공했습니다.");
 
     private final String code;
     private final String message;
